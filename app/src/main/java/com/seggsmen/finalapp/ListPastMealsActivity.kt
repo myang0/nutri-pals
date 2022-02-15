eea664b9e15d33499be939d796e858a29f778784
myang0 <mwyang99@gmail.com>
2022-02-15 14:58:53 -0800

    Add past meal view page

--- app/src/main/java/com/seggsmen/finalapp/ListPastMealsActivity.kt
+++ app/src/main/java/com/seggsmen/finalapp/ListPastMealsActivity.kt
@@ -1,5 +1,6 @@
 package com.seggsmen.finalapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
@@ -10,17 +11,26 @@ class ListPastMealsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_past_meals)
+
+        binding = ActivityListPastMealsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         var actionBar : ActionBar? = this.supportActionBar
 
         actionBar?.setTitle("Meal Library")
         actionBar?.setSubtitle("Saved Meals")
         actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.pastMealListing.setOnClickListener { navigateToPastMealView() }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    private fun navigateToPastMealView() {
+        val intent: Intent = Intent(this, ViewPastMealActivity::class.java)
+        startActivity(intent)
     }
 }
