90d8cb8d5ae014f8c63ed74f3b94977206543c9d
kqrse <xkarsonnguyen@gmail.com>
2022-02-15 15:27:38 -0800

    track meal menu

--- app/src/main/java/com/seggsmen/finalapp/MainActivity.kt
+++ app/src/main/java/com/seggsmen/finalapp/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.seggsmen.finalapp
 
 import android.content.Intent
+import android.graphics.RenderEffect
+import android.graphics.Shader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.seggsmen.finalapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +24,11 @@ class MainActivity : AppCompatActivity() {
 
         binding.pastPetsButton.setOnClickListener { navigateToPastPets() }
         binding.pastMealsButton.setOnClickListener { navigateToPastMeals() }
+        binding.trackMealButton.setOnClickListener { showBlurAndTrackButton() }
+        binding.trackMealOverlayLayer.setOnClickListener { cancelTrackMealOverlay() }
+        binding.cancelTrackMealButton.setOnClickListener { cancelTrackMealOverlay() }
+        binding.newMealButton.setOnClickListener { navigateToNewMeal() }
+        binding.savedMealsButton.setOnClickListener { navigateToSavedMeals() }
     }
 
     private fun navigateToPastPets() {
@@ -28,5 +38,29 @@ class MainActivity : AppCompatActivity() {
 
     private fun navigateToPastMeals() {
         Toast.makeText(this, "going to past meals", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showBlurAndTrackButton() {
+        binding.trackMealUnderlay.alpha = 0.5f
+        binding.trackMealOverlayLayer.visibility = View.VISIBLE
+        binding.pastPetsButton.isClickable = false
+        binding.pastMealsButton.isClickable = false
+        binding.trackMealButton.isClickable = false
+    }
+
+    private fun navigateToNewMeal() {
+        Toast.makeText(this, "going to new meals", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun navigateToSavedMeals() {
+        Toast.makeText(this, "going to saved meals", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun cancelTrackMealOverlay() {
+        binding.trackMealUnderlay.alpha = 1f
+        binding.trackMealOverlayLayer.visibility = View.GONE
+        binding.pastPetsButton.isClickable = true
+        binding.pastMealsButton.isClickable = true
+        binding.trackMealButton.isClickable = true
     }
 }
